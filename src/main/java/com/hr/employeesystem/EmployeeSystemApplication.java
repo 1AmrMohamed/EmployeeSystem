126c448872c5c5a3451f893c31c8e2adbe3902c5
Amr Hashem <amr.hashem@assetdev.com>
2024-05-03 18:14:02 +0300

    this is my temp and test version v1.0 beata

---
+++ src/main/java/com/hr/employeesystem/EmployeeSystemApplication.java
@@ -0,0 +1,17 @@
+package com.hr.employeesystem;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeSystemApplication.class, args);
+
+
+        System.out.println("hello world..");
+    }
+
+}
